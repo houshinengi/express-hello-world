53f174b739a6da1722a07303c3b118c53c21bb2b
houshinengi <370970068@qq.com>
2023-06-12 16:10:37 +0800

    Update service.js

--- service.js
+++ service.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 10000;
 const dataFilePath = "data.json";
 
 // 静态文件中间件，将public文件夹作为静态资源提供
