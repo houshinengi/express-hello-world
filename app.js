53e8a649d27ee4a82370c3d2754e0366b9e43dec
houshinengi <370970068@qq.com>
2023-06-12 16:27:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3030;
 const dataFilePath = "data.json";
 
-// 静态文件中间件，将public文件夹作为静态资源提供
 app.use(express.static("public"));
 
-// 处理GET请求，返回存储的数字
 app.get("/", (req, res) => {
   const data = getDataFromFile();
-  res.send(`当前数字为：${data.number}`);
+  res.send(data.number.toString());
 });
 
-// 处理POST请求，增加数字
 app.post("/increment", (req, res) => {
   const data = getDataFromFile();
   data.number += 1;
@@ -21,13 +18,11 @@ app.post("/increment", (req, res) => {
   res.send("数字已增加");
 });
 
-// 读取JSON文件的数据
 function getDataFromFile() {
   const data = fs.readFileSync(dataFilePath, "utf-8");
   return JSON.parse(data);
 }
 
-// 将数据保存到JSON文件
 function saveDataToFile(data) {
   fs.writeFileSync(dataFilePath, JSON.stringify(data));
 }
