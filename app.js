afe77561e7425416d1150b90a3d6a6aa594b44ba
houshinengi <370970068@qq.com>
2023-06-12 16:41:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3030;
-const dataFilePath = "data.json";
+const fs = require('fs');
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  const data = getDataFromFile();
-  res.send(data.number.toString());
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/updateCount', (req, res) => {
+    const { count } = req.body;
+
+    // 将 count 写入 JSON 文件
+    fs.writeFile('data.json', JSON.stringify({ count }), err => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ message: 'Error saving count' });
+        } else {
+            res.json({ message: 'Count updated successfully' });
+        }
+    });
 });
-
-app.post("/increment", (req, res) => {
-  const data = getDataFromFile();
-  data.number += 1;
-  saveDataToFile(data);
-  res.send("数字已增加");
-});
-
-function getDataFromFile() {
-  const data = fs.readFileSync(dataFilePath, "utf-8");
-  return JSON.parse(data);
-}
-
-function saveDataToFile(data) {
-  fs.writeFileSync(dataFilePath, JSON.stringify(data));
-}
 
 app.listen(PORT, () => {
-  console.log(`服务器已启动，端口号：${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
