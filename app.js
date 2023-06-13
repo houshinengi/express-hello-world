50c088cadd39f7e1889f86ddc0c171a504d1c541
houshinengi <370970068@qq.com>
2023-06-13 22:14:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const path = require('path'); // 添加这一行
+const path = require('path');
 
 const dataFile = 'data.json';
 
 // 设置静态文件目录
 app.use(express.static('public'));
 
-
 // 设置路由处理更新数字请求
 app.get('/update-number', (req, res) => {
-  // 读取当前数字
   fs.readFile(dataFile, (err, data) => {
     if (err) {
       console.error('Error reading file:', err);
@@ -19,31 +17,25 @@ app.get('/update-number', (req, res) => {
     }
 
     let number = JSON.parse(data).number;
-
-    // 增加数字
     number++;
 
-    // 更新json文件
     fs.writeFile(dataFile, JSON.stringify({ number }), err => {
       if (err) {
         console.error('Error writing file:', err);
         return res.status(500).json({ error: 'Internal Server Error' });
       }
 
-      // 返回更新后的数字
       res.json({ number });
     });
   });
 });
 
 // 设置路由处理根目录请求
-app.get('/', (req, res) => {
-  // 检查是否存在 data.json 文件，如果不存在或为空则创建初始的 JSON 数据
+app.get('/get-number', (req, res) => {
   if (!fs.existsSync(dataFile) || fs.readFileSync(dataFile, 'utf8').trim() === '') {
     fs.writeFileSync(dataFile, JSON.stringify({ number: 0 }));
   }
 
-  // 读取当前数字
   fs.readFile(dataFile, (err, data) => {
     if (err) {
       console.error('Error reading file:', err);
@@ -52,11 +44,14 @@ app.get('/', (req, res) => {
 
     let number = JSON.parse(data).number;
 
-    // 返回 index.html 文件
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.json({ number });
   });
 });
 
+// 设置路由处理根目录请求
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // 启动服务器
 app.listen(3000, () => {
