06cd368add52ee6b652df9f02c4b6cdfe150299b
houshinengi <370970068@qq.com>
2023-06-13 19:17:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,22 @@ app.get('/update-number', (req, res) => {
   });
 });
 
+// 设置路由处理根目录请求
+app.get('/', (req, res) => {
+  // 读取当前数字
+  fs.readFile(dataFile, (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    let number = JSON.parse(data).number;
+
+    // 渲染模板并传递数字
+    res.render('index', { number });
+  });
+});
+
 // 启动服务器
 app.listen(3000, () => {
   console.log('Server started on port 3000');
