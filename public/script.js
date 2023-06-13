4eda76efe13e0dbc67af73ffcb64492c6e2bf99b
houshinengi <370970068@qq.com>
2023-06-13 19:02:28 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,26 +1,16 @@
- <script>
-        const qiao = document.getElementById('qiao');
-        const gongde = document.getElementById('gongde');
-        let count = parseInt(gongde.textContent);
+const qiaoDiv = document.getElementById('qiao');
+const gongdeSpan = document.getElementById('gongde');
 
-        qiao.addEventListener('click', () => {
-            count++;
-            gongde.textContent = count;
-
-            // 将 count 数据发送给服务器
-            fetch('/updateCount', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ count })
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-        });
-    </script>
+// 监听点击事件
+qiaoDiv.addEventListener('click', () => {
+  // 发送请求到后端
+  fetch('/update-number')
+    .then(response => response.json())
+    .then(data => {
+      // 更新数字显示
+      gongdeSpan.textContent = data.number;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
